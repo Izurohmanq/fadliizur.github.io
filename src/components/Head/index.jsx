1cce96753279362df5eca84686a4c7ffabd43727
Izurohmanq <marqozur@gmail.com>
2024-08-24 21:42:54 +0700

    Update for SEO part 2

--- src/components/Head/index.jsx
+++ src/components/Head/index.jsx
@@ -1,11 +1,13 @@
 import { Helmet } from "react-helmet-async";
+import SkullFoto from "../../assets/skull.png";
+
 
 export default function Head({ title,desc, keywords }) {
   return (
     <>
       <Helmet>
         <meta charset="UTF-8" />
-        <link rel="icon" type="image/svg+xml" href="/src/assets/skull.png" />
+        <link rel="icon" type="image/svg+xml" href={SkullFoto} />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>{title}</title>
         <meta name="description" content={desc} />
